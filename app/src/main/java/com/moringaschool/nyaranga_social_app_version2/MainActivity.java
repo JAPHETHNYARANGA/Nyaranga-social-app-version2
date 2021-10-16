7ea2bef55e1de31d8b97a469a317e7d7c378e345
JAPHETHNYARANGA <nyaranga4@gmail.com>
2021-10-16 10:25:45 +0300

    Added twitter login session

--- app/src/main/java/com/moringaschool/nyaranga_social_app_version2/MainActivity.java
+++ app/src/main/java/com/moringaschool/nyaranga_social_app_version2/MainActivity.java
@@ -4,7 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
 import com.twitter.sdk.android.core.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthToken;
+import com.twitter.sdk.android.core.TwitterCore;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
 import com.twitter.sdk.android.core.identity.TwitterLoginButton;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,5 +22,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Twitter.initialize(this);
         setContentView(R.layout.activity_main);
+
+        loginButton = (TwitterLoginButton)findViewById(R.id.login_button);
+        loginButton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+                TwitterSession session = TwitterCore.getInstance().getSessionManager().getActiveSession();
+                TwitterAuthToken authToken=session.getAuthToken();
+
+                loginMethod(session);
+            }
+
+            @Override
+            public void failure(TwitterException exception) {
+
+            }
+        });
     }
 }
