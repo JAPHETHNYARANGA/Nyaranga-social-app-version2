d185aa178676388f85c786084a44096d90b76197
JAPHETHNYARANGA <nyaranga4@gmail.com>
2021-10-16 10:21:55 +0300

    Initialized twitter

--- app/src/main/java/com/moringaschool/nyaranga_social_app_version2/MainActivity.java
+++ app/src/main/java/com/moringaschool/nyaranga_social_app_version2/MainActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.twitter.sdk.android.core.Twitter;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+
 public class MainActivity extends AppCompatActivity {
+
+    TwitterLoginButton loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Twitter.initialize(this);
         setContentView(R.layout.activity_main);
     }
 }
