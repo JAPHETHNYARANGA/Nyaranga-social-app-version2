61adecdba93a42f7fad1899bd740b62f6113709d
JAPHETHNYARANGA <nyaranga4@gmail.com>
2021-10-17 14:17:23 +0300

    Started the whole project again

--- app/src/main/java/com/moringaschool/nyaranga_social_app_version2/ItemResult.java
+++
@@ -1,4 +0,0 @@
-package com.moringaschool.nyaranga_social_app_version2;
-
-public class ItemResult {
-}
--- app/src/main/java/com/moringaschool/nyaranga_social_app_version2/controller/DetailActivity.java
+++ app/src/main/java/com/moringaschool/nyaranga_social_app_version2/controller/DetailActivity.java
@@ -3,67 +3,18 @@ package com.moringaschool.nyaranga_social_app_version2.controller;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ShareCompat;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.text.util.Linkify;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toolbar;
 
-import com.bumptech.glide.Glide;
+import android.os.Bundle;
+
 import com.moringaschool.nyaranga_social_app_version2.R;
 
 public class DetailActivity extends AppCompatActivity {
-    TextView Link, Username;
-    Toolbar mActionBarToolbar;
-    ImageView imageView;
 
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        imageView = (ImageView) findViewById(R.id.user_image_header);
-        Username = (TextView) findViewById(R.id.userName);
-
-        Link = (TextView) findViewById(R.id.link);
-
-        String username = getIntent().getExtras().getString("login");
-        String avatarUrl = getIntent().getExtras().getString("avatar_url");
-        String link = getIntent().getExtras().getString("html_url");
-
-        Link.setText(link);
-        Linkify.addLinks(Link, Linkify.WEB_URLS);
-
-        Username.setText(username);
-        Glide.with(this)
-                .load(avatarUrl)
-                .placeholder(R.drawable.load)
-                .into(imageView);
-
-        getSupportActionBar().setTitle("Details Activity");
     }
 
-    private Intent createShareForcastIntent(){
-        String username = getIntent().getExtras().getString("login");
-        String link = getIntent().getExtras().getString("link");
-        Intent shareIntent = ShareCompat.IntentBuilder.from(this)
-                .setType("text/plain")
-                .setText("Check out this awesome developer @" + username + ", " + link)
-                .getIntent();
-        return shareIntent;
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu){
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.detail, menu);
-        MenuItem menuItem = menu.findItem(R.id.action_share);
-        menuItem.setIntent(createShareForcastIntent());
-        return true;
-    }
 }
