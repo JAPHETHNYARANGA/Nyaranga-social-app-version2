80dce2ff21acba555473fc3507374aa24066d6c4
JAPHETHNYARANGA <nyaranga4@gmail.com>
2021-10-18 10:59:20 +0300

    Created contact-us activity

---
+++ app/src/main/java/com/moringaschool/nyaranga_social_app_version2/controller/ContactUs.java
@@ -0,0 +1,14 @@
+package com.moringaschool.nyaranga_social_app_version2.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ContactUs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_us);
+    }
+}
